3a0ce3bed938097780135da7ebdcd75319e93ba9
Marcelo Martins <m.a.martins@live.com>
2020-01-17 15:25:57 -0300

    Added animated transitions

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, RouteComponentProps, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Header from "./Header";
 import AdminPage from "./admin/AdminPage";
 import ProductsPage from "./products/ProductsPage";
@@ -7,23 +8,33 @@ import ProductPage from "./products/ProductPage";
 import LoginPage from "./LoginPage";
 import NotFoundPage from "./NotFoundPage";
 
-const Routes = () => {
+const Routes: React.FC<RouteComponentProps> = (props) => {
   const [loggedIn] = React.useState(true);
 
   return (
+    <div>
+      <Header/>
+      <TransitionGroup>
+        <CSSTransition key={props.location.key} timeout={500} classNames="animate">
+          <Switch>
+            <Redirect exact from="/" to="/products"/>
+            <Route path="/products" exact component={ProductsPage}/>
+            <Route path="/products/:id" component={ProductPage}/>
+            <Route path="/admin">{loggedIn ? <AdminPage/> : <Redirect to="/login"/>}</Route>
+            <Route path="/login" component={LoginPage}/>
+            <Route component={NotFoundPage}/>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  )
+};
+
+const RoutesWrapper = () => {
+  return (
     <Router>
-      <div>
-        <Header/>
-        <Switch>
-          <Redirect exact from="/" to="/products"/>
-          <Route path="/products" exact component={ProductsPage}/>
-          <Route path="/products/:id" component={ProductPage}/>
-          <Route path="/admin">{loggedIn ? <AdminPage/> : <Redirect to="/login"/>}</Route>
-          <Route path="/login" component={LoginPage}/>
-          <Route component={NotFoundPage}/>
-        </Switch>
-      </div>
+      <Route component={Routes}/>
     </Router>
   )
 };
-export default Routes
+export default RoutesWrapper
