b8fbf0e160cf7936762ea9b4554d6cf47fb3876b
Marcelo Martins <m.a.martins@live.com>
2020-01-22 16:12:16 -0300

    Implemented 'like button' feature

--- src/products/Product.tsx
+++ src/products/Product.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { IProduct } from "./products-data";
 import { Tabs, Tab } from "./Tabs";
 import withLoader from "../withLoader";
@@ -9,11 +9,48 @@ type Props = {
   onAddToBasket: () => void
 }
 
+type LikeState = {
+  likes: number,
+  lastLike: Date | null
+}
+
+const initialLikeState: LikeState = {
+  likes: 0,
+  lastLike: null
+};
+
+enum LikeActionTypes {
+  LIKE = "LIKE"
+}
+
+type LikeAction = {
+  type: LikeActionTypes.LIKE,
+  now: Date
+}
+
+type LikeActions = LikeAction;
+
+const reducer = (state = initialLikeState, action: LikeActions) => {
+  if (action.type === LikeActionTypes.LIKE) {
+    return {
+      ...state,
+      likes: state.likes + 1, lastLike: action.now
+    };
+  } else {
+    return state;
+  }
+};
+
 const Product: React.FC<Props> = (props) => {
+  const [{ likes, lastLike }, dispatch] = useReducer(reducer, initialLikeState);
   const product = props.product;
 
   const handleAddClick = () => {
     props.onAddToBasket();
+  };
+
+  const handleLikeClick = () => {
+    dispatch({ type: LikeActionTypes.LIKE, now: new Date() });
   };
 
   if (!product) {
@@ -47,6 +84,14 @@ const Product: React.FC<Props> = (props) => {
       {!props.inBasket && (
         <button onClick={handleAddClick}>Add to basket</button>
       )}
+      <div className="like-container">
+        {likes > 0 && (
+          <div>{`I like this x ${likes}, last as ${lastLike}`}</div>
+        )}
+        <button onClick={handleLikeClick}>
+          {likes > 0 ? "Like again" : "Like"}
+        </button>
+      </div>
     </>
   )
 };
